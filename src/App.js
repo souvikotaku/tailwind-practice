c73dc720c8e7c9cd0b962b4e73ebc140d94dea70
souvikotaku <manson1989@>
2023-12-28 09:23:27 +0530

    chnges

--- src/App.js
+++ src/App.js
@@ -4,7 +4,11 @@ import "./App.css";
 function App() {
   return (
     <div className="bg-slate-900 grid h-screen place-content-center">
-      <h1 className="text-7xl text-center text-blue-400">Hello World</h1>
+      <div className="bg-white p-10 rounded-lg group hover:cursor-pointer hover:bg-red-500">
+        <h1 className="text-7xl text-center text-blue-400 group-hover:text-white">
+          Hello World
+        </h1>
+      </div>
     </div>
   );
 }
